d424e6148b6d6f3038e98748d264bbbdb6188c9c
Steven Gutierrez <stevogbmv@gmail.com>
2022-12-15 17:42:46 -0800

    commit tree f9ae381332f972519e1a304d99a3a7bbd1f00689

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
+    //Holds all of the JS for event
+    const inputForm = document.querySelector('form');
+    //Grabs form from HTML doc
+    inputForm.addEventListener('submit', (event) => {
+        //submit event listener creates inner HTML & grabs data from db.json
+        event.preventDefault();
+        // DON'T FORGET THIS!
+        const textInput = document.getElementById('searchByID');
+        // Grabs text that has been submitted into the web page
+        fetch(`http://localhost:3000/movies/${textInput.value}`)
+        //fetch request from api, single object return given by input
+        .then(response => response.json())
+        // converts string to Java Script object
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.innerText = data.title;
+            summary.innerText = data.summary
+        })
+    })
   
 }
 
 document.addEventListener('DOMContentLoaded', init);
+//Runs after HTML is loaded, calls function 'init'
